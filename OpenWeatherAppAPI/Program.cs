61c8052c67ec666a18180a935227a1f4fce3c199
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2022-07-28 13:05:13 -0700

    API&JsonExercise2

---
+++ OpenWeatherAppAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Please enter ypour API Key:");
+            var api_Key = Console.ReadLine();
+            while(true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter the city name:");
+                var city_name = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you likke to choose a diferent city?");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+                if (userInput.ToLower() == "no")
+                {
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
